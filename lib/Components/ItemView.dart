e8ecf82451c4c762683c133845a2927163a0cfa9
Tim-dev-hub <timurteresenko@gmail.com>
2022-11-30 17:57:05 +0200

    Implement displayng of best item and properly calculate %

--- lib/Components/ItemView.dart
+++ lib/Components/ItemView.dart
@@ -6,111 +6,147 @@ class ItemView extends StatelessWidget {
   const ItemView({
     required this.key,
     required this.item,
+    required this.meta,
   }) : super(key: key);
 
   final Item item;
   final Key key;
+  final ItemCalculationResult meta;
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-        future: DynamicColorPlugin.getCorePalette(),
-        builder: (_, paletteData) {
-          var palette = paletteData.data;
+      future: DynamicColorPlugin.getCorePalette(),
+      builder: (_, paletteData) {
+        var palette = paletteData.data;
 
-          Widget buildLine({
-            required IconData icon,
-            required String name,
-            required bool isHeadline,
-            String? badge,
-            required String value,
-          }) {
-            var fontStyle = Theme.of(context).textTheme.bodyLarge?.copyWith(
-                  color: isHeadline
-                      ? null
-                      : Theme.of(context).textTheme.headlineMedium?.color,
-                  fontWeight: isHeadline ? FontWeight.bold : null,
-                );
+        Widget buildLine({
+          required IconData icon,
+          required String name,
+          required bool isHeadline,
+          String? badge,
+          required String value,
+        }) {
+          var headColor = meta.isBest
+              ? Color(palette?.primary.get(20) ?? 0)
+              : Color(palette?.neutral.get(20) ??
+                  Theme.of(context).textTheme.headlineMedium?.color?.value ??
+                  0);
+          var secondaryColor = meta.isBest
+              ? Color(palette?.primary.get(30) ?? 0)
+              : Color(palette?.neutral.get(30) ??
+                  Theme.of(context).textTheme.headlineMedium?.color?.value ??
+                  0);
 
-            var textBackColor = Color(palette?.primary.get(70) ??
-                Theme.of(context).colorScheme.primary.value);
+          var fontStyle = Theme.of(context).textTheme.bodyLarge?.copyWith(
+                color: isHeadline ? headColor : secondaryColor,
+                fontWeight: isHeadline ? FontWeight.bold : null,
+              );
 
-            return Container(
-              padding: const EdgeInsets.symmetric(vertical: 1),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Icon(
-                        icon,
-                        color: fontStyle?.color,
-                        size: (fontStyle?.fontSize ?? 10) * 1.25,
-                      ),
-                      Text(
-                        name,
-                        style: fontStyle,
-                      ),
-                    ],
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 4),
-                    decoration: !isHeadline ? null : BoxDecoration(
-                      color: textBackColor,
-                      borderRadius: const BorderRadius.all(
-                        Radius.circular(8),
-                      ),
+          var textBackColor = Color(palette?.primary.get(meta.isBest ? 70 : 90) ??
+              Theme.of(context).colorScheme.primary.value);
+
+          return Container(
+            padding: const EdgeInsets.symmetric(vertical: 1),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      icon,
+                      color: fontStyle?.color,
+                      size: (fontStyle?.fontSize ?? 10) * 1.25,
                     ),
-                    child: Text(
-                      value,
+                    Text(
+                      name,
                       style: fontStyle,
                     ),
-                  )
-                ],
-              ),
-            );
-          }
-
-          var cardColor = Color(palette?.primary?.get(95) ??
-              Theme.of(context).colorScheme.surfaceVariant.value);
-
-          return Dismissible(
-            key: key,
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 11),
-              decoration: BoxDecoration(
-                color: cardColor,
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(10),
+                    if (badge != null && !meta.isBest)
+                      Transform.translate(
+                        offset: const Offset(0, -5),
+                        child: Text(
+                          badge!,
+                          style: TextStyle(
+                            fontSize: 11,
+                            fontWeight: FontWeight.bold,
+                            color: Color(palette?.tertiary.get(40) ?? 0),
+                          ),
+                        ),
+                      )
+                  ],
                 ),
-              ),
-              child: Column(
-                children: [
-                  buildLine(
-                    icon: Icons.add_alert_rounded,
-                    name: "Цена за кг",
-                    isHeadline: true,
-                    badge: "100%",
-                    value:
-                        ((1000 / item.weight) * item.price).toStringAsFixed(2),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  decoration: !isHeadline
+                      ? null
+                      : BoxDecoration(
+                          color: textBackColor,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(8),
+                          ),
+                        ),
+                  child: Text(
+                    value,
+                    style: fontStyle,
                   ),
-                  buildLine(
-                    icon: Icons.ac_unit_rounded,
-                    name: "Вес",
-                    isHeadline: false,
-                    value: item.weight.toStringAsFixed(2),
-                  ),
-                  buildLine(
-                    icon: Icons.price_change_rounded,
-                    name: "Цена",
-                    isHeadline: false,
-                    value: item.price.toStringAsFixed(2),
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
           );
-        });
+        }
+
+        var cardColor = Color(palette?.primary?.get(meta.isBest ? 80 : 95) ??
+            Theme.of(context).colorScheme.surfaceVariant.value);
+
+        return Dismissible(
+          key: key,
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 11),
+            decoration: BoxDecoration(
+              color: cardColor,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            child: Column(
+              children: [
+                buildLine(
+                  icon: Icons.add_alert_rounded,
+                  name: "Цена за кг",
+                  isHeadline: true,
+                  badge: "${meta.percent}%",
+                  value: ((1000 / item.weight!) * item.price!).toStringAsFixed(2),
+                ),
+                buildLine(
+                  icon: Icons.ac_unit_rounded,
+                  name: "Вес",
+                  isHeadline: false,
+                  value: item.weight!.toStringAsFixed(2),
+                ),
+                buildLine(
+                  icon: Icons.price_change_rounded,
+                  name: "Цена",
+                  isHeadline: false,
+                  value: item.price!.toStringAsFixed(2),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
+}
+
+class ItemCalculationResult {
+  const ItemCalculationResult({
+    required this.isBest,
+    required this.percent,
+    required this.pricePerKilogram,
+  });
+  final int percent;
+  final bool isBest;
+  final double pricePerKilogram;
 }
--- lib/Components/Screens/MainScreen.dart
+++ lib/Components/Screens/MainScreen.dart
@@ -11,18 +11,27 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
+    int updateIteration = 0;
     return StreamBuilder(
       stream: ItemController.mainScreenItems,
       builder: (_, data) {
+        updateIteration++;
         if (data.data != null) {
           var items = data.data!;
+          var bestItem = findBestSell(items);
+          var bestItemPrice = getPricePerKilogram(bestItem);
 
           return ListView(
             children: [
               for (var i in items)
                 ItemView(
-                  key: Key(items.indexOf(i).toString()),
+                  key: Key('$updateIteration + ${items.indexOf(i).toString()}'),
                   item: i,
+                  meta: ItemCalculationResult(
+                    isBest: bestItem == i,
+                    pricePerKilogram: getPricePerKilogram(i),
+                    percent: (getPricePerKilogram(i) / bestItemPrice * 100).toInt() ,
+                  ),
                 )
             ],
           );
@@ -32,4 +41,22 @@ class _MainScreenState extends State<MainScreen> {
       },
     );
   }
+
+  double getPricePerKilogram(Item item) {
+    return ((1000 / item.weight!) * item.price!);
+  }
+
+  Item findBestSell(List<Item> items) {
+    Item best = items.first;
+
+    for (var item in items) {
+      if (getPricePerKilogram(item) < getPricePerKilogram(best)) {
+        best = item;
+      }
+    }
+
+    return best;
+  }
+
+  //((1000 / item.weight) * item.price).toStringAsFixed(2)
 }
